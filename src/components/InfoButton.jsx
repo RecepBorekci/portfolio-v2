6da9981eafed55b4e79f188361388e5ef75bfa62
RecepBorekci <recepborekci29@gmail.com>
2025-07-02 22:19:32 +0300

    refactor: Refactor InfoButton and InfoMenu to use NavLink for navigation

--- src/components/InfoButton.jsx
+++ src/components/InfoButton.jsx
@@ -1,21 +1,25 @@
-export default function InfoButton({ icon, label }) {
+import { NavLink } from "react-router-dom";
+
+export default function InfoButton({ icon, label, path }) {
   return (
-    <button
+    <NavLink
+      to={path}
+      end
       className="
-      flex items-center gap-2 
-      justify-center
-      lg:flex-col xl:flex-row
-      bg-white/10 hover:bg-white/20 text-white border border-white/20
-      rounded-md 
-      
-      px-4 py-2 
-      text-[3.5vw] lg:text-[1.2vw] xl:text-[0.9vw] 
-      min-w-[80px] lg:min-w-[6vw] xl:min-w-[6vw]
-      min-h-[40px] lg:min-h-[8vw] xl:min-h-[3vw]
-      "
+    flex items-center gap-2 
+    justify-center
+    lg:flex-col xl:flex-row
+    bg-white/10 hover:bg-white/20 text-white border border-white/20
+    rounded-md 
+    
+    px-4 py-2 
+    text-[3.5vw] lg:text-[1.2vw] xl:text-[0.9vw] 
+    min-w-[80px] lg:min-w-[6vw] xl:min-w-[6vw]
+    min-h-[40px] lg:min-h-[8vw] xl:min-h-[3vw]
+    "
     >
       {icon}
       <span>{label}</span>
-    </button>
+    </NavLink>
   );
 }
--- src/components/InfoMenu.jsx
+++ src/components/InfoMenu.jsx
@@ -6,16 +6,16 @@ import {
   FaUsers,
   FaTrophy,
   FaFlag,
+  FaHome,
 } from "react-icons/fa";
 
 export default function InfoMenu() {
   const buttonData = [
-    { icon: <FaUser />, label: "Profile" },
-    { icon: <FaCalendarAlt />, label: "Season" },
-    { icon: <FaChartBar />, label: "Stats" },
-    { icon: <FaUsers />, label: "Team" },
-    { icon: <FaTrophy />, label: "League" },
-    { icon: <FaFlag />, label: "Country" },
+    { icon: <FaHome />, label: "Main", path: "/" },
+    { icon: <FaUser />, label: "Profile", path: "/profile" },
+    { icon: <FaCalendarAlt />, label: "Season", path: "/season" },
+    { icon: <FaChartBar />, label: "Stats", path: "/stats" },
+    { icon: <FaUsers />, label: "Team", path: "/team" },
   ];
 
   return (
@@ -23,14 +23,24 @@ export default function InfoMenu() {
       {/* Left side of screen (xl only) */}
       <div className="hidden xl:flex xl:flex-col fixed left-4 top-1/2 -translate-y-1/2 gap-4 z-50">
         {buttonData.map((button, idx) => (
-          <InfoButton key={idx} icon={button.icon} label={button.label} />
+          <InfoButton
+            key={idx}
+            icon={button.icon}
+            label={button.label}
+            path={button.path}
+          />
         ))}
       </div>
 
       {/* Bottom center for lg only */}
       <div className="hidden lg:flex xl:hidden fixed bottom-4 left-1/2 -translate-x-1/2 gap-2 z-50">
         {buttonData.map((button, idx) => (
-          <InfoButton key={idx} icon={button.icon} label={button.label} />
+          <InfoButton
+            key={idx}
+            icon={button.icon}
+            label={button.label}
+            path={button.path}
+          />
         ))}
       </div>
     </>
