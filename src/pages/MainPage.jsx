6e3faf8a435d9121744c466a151187f0867e6509
RecepBorekci <recepborekci29@gmail.com>
2025-07-02 22:20:09 +0300

    feat: Add placeholder pages for main sections

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,54 @@
+import Platform from "../components/Platform";
+import PlayerCard from "../components/PlayerCard";
+import SidePanel from "../components/SidePanel";
+import InfoMenu from "../components/InfoMenu";
+
+function MainPage() {
+  return (
+    <div
+      className="
+    min-h-screen overflow-hidden relative 
+    flex flex-col items-center justify-center gap-10
+  "
+    >
+      {/* Container for screens below xl - maintains existing flexbox layout */}
+      <div className="xl:hidden relative w-full max-w-[1280px] px-4 flex items-center justify-around">
+        {/* Left Panel */}
+        <div className="hidden lg:block self-end">
+          <SidePanel />
+        </div>
+
+        {/* Player Card */}
+        <div className="flex justify-center ">
+          <PlayerCard />
+        </div>
+
+        {/* Right Panel */}
+        <div className="hidden lg:block self-end">
+          <SidePanel mirrored />
+        </div>
+      </div>
+
+      {/* Container for xl+ screens - new relative positioned container */}
+      <div className="hidden xl:block relative w-full max-w-[1678px] h-[800px]">
+        {/* Platform positioned at bottom */}
+        <Platform />
+
+        {/* Left Panel - absolutely positioned */}
+        <div className="hidden lg:block">
+          <SidePanel />
+        </div>
+
+        {/* Player Card - absolutely positioned */}
+        <PlayerCard />
+
+        {/* Right Panel - absolutely positioned */}
+        <div className="hidden lg:block">
+          <SidePanel mirrored />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
---
+++ src/pages/StatsPage.jsx
@@ -0,0 +1,13 @@
+function StatsPage() {
+  // This Page will contain the information about my skills in general presented as soccer player stats format with ratings etc.
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="bg-white rounded-xl shadow-lg p-8 flex flex-col items-center">
+        <h1 className="text-4xl font-bold">Stats</h1>
+      </div>
+    </div>
+  );
+}
+
+export default StatsPage;
