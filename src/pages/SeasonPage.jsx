ae8371402bb6ec953c63d638c7fe3e8282e60d83
RecepBorekci <recepborekci29@gmail.com>
2025-07-07 22:22:36 +0300

    feat: Add ExperienceCard Component and Update SeasonPage to Display Experiences
    - Introduced ExperienceCard component for showcasing individual work experiences.
    - Updated portfolio.js to include detailed experience data for multiple roles.
    - Modified SeasonPage to render ExperienceCard components dynamically using the new experience data.
    - Enhanced styling for improved visual presentation of experiences.

--- src/pages/SeasonPage.jsx
+++ src/pages/SeasonPage.jsx
@@ -1,10 +1,17 @@
+import portfolioData from "../data/portfolio";
+import ExperienceCard from "../components/ExperienceCard";
+
 function SeasonPage() {
-  // This Page will contain the information about my past experiences and achievements in seasons format.
+  const seasonPageData = portfolioData.seasonPageData;
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="bg-white rounded-xl shadow-lg p-8 flex flex-col items-center">
-        <h1 className="text-4xl font-bold">Season</h1>
+    <div className="min-h-screen w-full flex items-center justify-center px-4 py-12 text-white">
+      <div className="bg-gradient-to-br from-black/60 via-gray-900/60 to-yellow-900/30 shadow-2xl border border-yellow-400/10 rounded-xl w-full container mx-auto p-10 ">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-[1280px] mx-auto lg:gap-10">
+          {seasonPageData.map((experience) => (
+            <ExperienceCard key={experience.id} experience={experience} />
+          ))}
+        </div>
       </div>
     </div>
   );
