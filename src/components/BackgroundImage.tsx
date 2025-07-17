44d7ea51d3438ccb6cf60050f83f4a2951dec4d5
RecepBorekci <recepborekci29@gmail.com>
2025-07-17 18:20:28 +0300

    Refactor App Component and Update Routing Structure
    - Changed the document type declaration in index.html for HTML5 compliance.
    - Updated the title in index.html to reflect the new project name "My Portfolio".
    - Refactored App component to utilize a new BackgroundImage component for improved code organization.
    - Modularized routing by introducing a routes.tsx file, enhancing maintainability and readability.
    - Removed direct route definitions from App component, replacing them with a dynamic mapping from the routes array.

---
+++ src/components/BackgroundImage.tsx
@@ -0,0 +1,18 @@
+import clsx from "clsx";
+
+interface BackgroundImageProps {
+  isMainPage: boolean;
+}
+
+export default function BackgroundImage({ isMainPage }: BackgroundImageProps) {
+  return (
+    <img
+      src="/images/Stadium.jpg"
+      alt="stadium"
+      className={clsx(
+        "absolute top-0 left-0 w-full h-full object-cover xl:object-top z-0 transition-all duration-500",
+        isMainPage ? "" : "blur-md brightness-50"
+      )}
+    />
+  );
+}
