061cf7f92fb5b4c54d21f7e37eabfed597a77790
RecepBorekci <recepborekci29@gmail.com>
2025-08-01 21:35:17 +0300

    Update package dependencies and Vite configuration for deployment
    - Added `gh-pages` dependency to facilitate deployment to GitHub Pages.
    - Updated `package.json` with new scripts for predeploy and deploy processes.
    - Set the base path in `vite.config.ts` to ensure correct asset loading when hosted on GitHub Pages.
    - Updated `package-lock.json` to reflect the new dependency and version changes.

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio-v2',
   plugins: [react(), tailwindcss(), svgr()],
   resolve: {
     alias: {
