b2c9a06ae33326e09972291f7381a40c75559524
Recep Börekci <104196234+RecepBorekci@users.noreply.github.com>
2025-08-02 17:00:57 +0300

    GitHub Pages Deployment Migration (#49)
    * Update package dependencies and Vite configuration for deployment
    
    - Added `gh-pages` dependency to facilitate deployment to GitHub Pages.
    - Updated `package.json` with new scripts for predeploy and deploy processes.
    - Set the base path in `vite.config.ts` to ensure correct asset loading when hosted on GitHub Pages.
    - Enhanced `package-lock.json` to reflect the addition of new dependencies and updates to existing ones.
    
    * Switch from BrowserRouter to HashRouter for improved routing compatibility in main application file.
    
    * Update asset paths in web manifest, CSS, and data files for consistency
    
    - Changed asset paths in public/site.webmanifest to use relative paths for icons and screenshots.
    - Updated font file paths in src/index.css to utilize the new alias for assets.
    - Modified image paths in playerCard.ts and profile.ts to ensure they are relative, improving asset loading consistency across the application.
    
    * Refactor BackgroundImage Component to Use Data-Driven Approach
    
    - Updated BackgroundImage component to source image path and alt text from a new backgroundImage data file, enhancing maintainability and flexibility for future updates.
    
    * Switch from HashRouter to BrowserRouter with a base path for improved routing in main application file.

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio-v2/">
       <App />
     </BrowserRouter>
   </StrictMode>
