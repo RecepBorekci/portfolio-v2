b31d368cfe2e06f6e20e74a88f6d6b336473bd95
RecepBorekci <recepborekci29@gmail.com>
2025-08-02 14:37:32 +0300

    Switch from HashRouter to BrowserRouter with a base path for improved routing in main application file.

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/portfolio-v2/">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
