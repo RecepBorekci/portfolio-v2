4ba02deca04f0f17aecabdac1f3639881137365f
RecepBorekci <recepborekci29@gmail.com>
2025-08-03 18:43:25 +0300

    Update Experience Components for Improved Responsiveness and Readability
    - Adjusted text sizes in ExperiencePageDescription and ExperiencePageLearned for better readability on smaller screens.
    - Modified layout in ExperiencePageHeader to enhance alignment and responsiveness, including centering text and adjusting grid structure for various screen sizes.

--- src/components/pages/experience/ExperiencePageHeader.tsx
+++ src/components/pages/experience/ExperiencePageHeader.tsx
@@ -5,18 +5,20 @@ interface ExperiencePageHeaderProps {
 }
 
 const ExperiencePageHeader = ({ experience }: ExperiencePageHeaderProps) => (
-  <div className="grid grid-cols-[auto_1fr] items-center gap-6">
+  <div className="grid grid-cols-1 items-center gap-4 sm:grid-cols-[auto_1fr]">
     <img
       src={experience.logo}
       alt={experience.logoAlt}
-      className="me-4 h-30 w-30 rounded-2xl border border-white/20 object-cover lg:me-10"
+      className="mx-auto h-30 w-30 rounded-2xl border border-white/20 object-cover sm:mx-0 sm:me-4 lg:me-10"
     />
-    <div className="flex flex-col justify-center gap-1">
-      <h1 className="text-2xl font-bold md:text-3xl">{experience.company}</h1>
-      <h2 className="text-xl font-semibold md:text-2xl">
+    <div className="flex flex-col items-center justify-center gap-1 sm:items-start">
+      <h1 className="text-center text-2xl font-bold sm:text-start md:text-3xl">
+        {experience.company}
+      </h1>
+      <h2 className="text-center text-xl font-semibold sm:text-start md:text-2xl">
         {experience.position}
       </h2>
-      <p className="text-md text-gray-400 md:text-lg">
+      <p className="text-md text-center text-gray-400 sm:text-start md:text-lg">
         From: {experience.startDate} <br />
         To: {experience.endDate}
       </p>
--- src/components/pages/experience/ExperiencePageLearned.tsx
+++ src/components/pages/experience/ExperiencePageLearned.tsx
@@ -7,7 +7,7 @@ const ExperiencePageLearned = ({
 }: ExperiencePageLearnedProps) => (
   <div className="mx-auto mt-6 grid max-w-[1280px] grid-cols-1 gap-2 sm:mt-8 xl:mt-10">
     <h3 className="text-xl font-bold md:text-2xl">What I Learned</h3>
-    <ul className="text-md list-disc pl-6 md:text-lg">
+    <ul className="list-disc pl-6 text-sm md:text-lg">
       {whatILearned.map((item, i) => (
         <li key={i}>{item}</li>
       ))}
