fe8b6de43c0a628e85886227230a720110ad7a45
RecepBorekci <recepborekci29@gmail.com>
2025-08-04 22:13:25 +0300

    Update TypeScript and Vite Configuration to Include Hooks Path
    - Added new path alias '@hooks/*' in tsconfig.json for improved module resolution.
    - Updated vite.config.ts to include the '@hooks' alias, enhancing organization and maintainability of imports.

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
       '@pages': path.resolve(__dirname, 'src/pages'),
       '@customTypes': path.resolve(__dirname, 'src/types'),
       '@utils': path.resolve(__dirname, 'src/utils'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
     },
   },
 });
